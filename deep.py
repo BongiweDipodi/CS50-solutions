d228b2e7494b1382d6634d1473f4ed574c7f8b50
Bongiwe Dipodi <156716077+BongiweDipodi@users.noreply.github.com>
2025-08-13 22:44:46 +0200

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,13 @@
+user = input("What is the answer to the greatest question of Life, the Universe, and Everything? ").lower().strip()
+
+if user == "42":
+    print("Yes")
+
+elif user == "forty-two":
+    print("Yes")
+
+elif user == "forty two":
+    print("Yes")
+
+else:
+    print("no")
